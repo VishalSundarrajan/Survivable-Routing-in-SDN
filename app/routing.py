4f5f2905bb088a7587419a841a6fd7436c905da0
Taha Khan1 <khantaha127@gmail.com>
2018-01-14 11:15:04 -0500

    modified routing

--- app/routing.py
+++ app/routing.py
@@ -15,7 +15,6 @@
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
@@ -23,33 +22,31 @@ from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 import logging
 import struct
-from ryu.base import app_manager
-from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
-from ryu.controller.handler import CONFIG_DISPATCHER
-from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-from ryu.lib.packet import ether_types
+from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER, CONFIG_DISPATCHER
 from ryu.lib.packet import ipv4
 from ryu.lib import mac
 from ryu.lib.packet import arp
 from ryu.topology import event, switches
 from ryu.topology.api import get_switch, get_link
+from ryu.controller import dpset
+from ryu.lib import dpid 
+from ryu.controller import handler
 
 
 class SimpleSwitch14(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    _CONTEXTS={'dpset': dpset.DPSet}
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch14, self).__init__(*args, **kwargs)
+        self.topology_api_app = self
         self.mac_to_port = {}
         self.switch_port_table = {}
         self.link_to_port = {} 
         self.interior_ports = {}
         self.access_ports = {}
         self.access_table={}
+        self.sw_list= {}
         
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -68,6 +65,23 @@ class SimpleSwitch14(app_manager.RyuApp):
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
+        
+    @set_ev_cls(ofp_event.EventOFPStateChange,
+                [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    def _state_change_handler(self, ev):
+      dp=ev.datapath
+      if ev.state == handler.MAIN_DISPATCHER:
+        self.sw_list[dp.id]=dp
+        msg='Join SW'
+      elif ev.state ==handler.DEAD_DISPATCHER:
+        ret = self.sw_list.pop(dp.id,None)
+        if ret is None:
+          msg='Leave unknown SW'
+          
+        else:
+          msg='Leave sw'
+      self.logger.info('dpid {} {} '.format(msg,self.sw_list))
+      self.logger.info("port state change event triggered")
 
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
@@ -92,7 +106,7 @@ class SimpleSwitch14(app_manager.RyuApp):
 
             for p in sw.ports:
                 self.switch_port_table[dpid].add(p.port_no)
-            self.logger.info(self.switch_port_table)
+            self.logger.info('Switch_port_table :{}'.format(self.switch_port_table))
         		
     		
     def create_interior_links(self, link_list):
@@ -100,21 +114,24 @@ class SimpleSwitch14(app_manager.RyuApp):
           Get links`srouce port to dst port  from link_list,
           link_to_port:(src_dpid,dst_dpid)->(src_port,dst_port)
       """
+     
+      
       for link in link_list:
           src = link.src
           dst = link.dst
           self.link_to_port[(src.dpid, dst.dpid)] = (src.port_no, dst.port_no)
-      
   
           # Find the access ports and interior ports
           if link.src.dpid in self.switches:
               self.interior_ports[link.src.dpid].add(link.src.port_no)
           if link.dst.dpid in self.switches:
               self.interior_ports[link.dst.dpid].add(link.dst.port_no)
-          self.logger.info('Link_to_Port {}'.format(self.link_to_port))
-          self.logger.info('Interior_Ports {}'.format(self.interior_ports))
+               
+      self.logger.info('Link_to_Port {}'.format(self.link_to_port))
+      self.logger.info('Interior_Ports {}'.format(self.interior_ports))
           
-    '''def create_access_ports(self):
+          
+    def create_access_ports(self):
       """
           Get ports without link into access_ports
       """
@@ -122,20 +139,26 @@ class SimpleSwitch14(app_manager.RyuApp):
           all_port_table = self.switch_port_table[sw]
           interior_port = self.interior_ports[sw]
           self.access_ports[sw] = all_port_table - interior_port
-      self.logger.info('Interiors_ports : {}'.format(self.interior_ports))'''
-          
+      self.logger.info('Access_ports : {}'.format(self.access_ports))
+        
+    events = [event.EventSwitchEnter,
+              event.EventSwitchLeave, event.EventPortAdd,
+              event.EventPortDelete, event.EventPortModify,
+              event.EventLinkAdd, event.EventLinkDelete]    
+              
+    
       
-    @set_ev_cls(event.EventSwitchEnter)
+    @set_ev_cls(events)
     def get_topology(self, ev):
     	"""
     		Get topology info and store it.
     	"""
-    	switch_list = get_switch(self, None)
+    	switch_list = get_switch(self.topology_api_app, None)
     	self.create_port_map(switch_list)
     	self.switches=self.switch_port_table.keys()
-    	links=get_link(self, None)
+    	links=get_link(self.topology_api_app, None)
     	self.create_interior_links(links)
-    	#self.create_access_ports()
+    	self.create_access_ports()
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
@@ -171,6 +194,19 @@ class SimpleSwitch14(app_manager.RyuApp):
         src = eth.src
         dpid = datapath.id
         arp_pkt=pkt.get_protocol(arp.arp)
+        data=None
+        buffer_id=msg.buffer_id
+        data=msg.data
+        if msg.buffer_id==ofproto.OFP_NO_BUFFER:
+            data=msg.data
+        actions=[]
+        
+        if isinstance(arp_pkt, arp.arp):
+          self.logger.debug("***********Arp Processing***********")
+          
+          
+          
+          
       
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:  # ignore lldp packet
         #self.logger.info("LLDP packet in %s %s %s %s", dpid, src, dst, in_port)
@@ -186,9 +222,15 @@ class SimpleSwitch14(app_manager.RyuApp):
           return
   
         if  dst==mac.BROADCAST_STR: # Handle ARP broadcast 	
-            self.logger.info('This is ARP broadcast received at port {} of switch {}'.format(in_port, datapath.id) )	
-                    #self.send_arpproxy()
-
+  		      self.logger.info('This is ARP broadcast received at port {} of switch {}'.format(in_port, datapath.id))	
+  		      datapath=self.sw_list[2]
+  		      self.logger.info("Datapath {}".format(datapath.id))
+  		      actions.append(datapath.ofproto_parser.OFPActionOutput(3))
+  		      self.logger.info("actions {}, bufferid {}, data {}".format(actions, buffer_id, data))
+  		      out = datapath.ofproto_parser.OFPPacketOut(datapath=datapath, in_port=ofproto.OFPP_CONTROLLER, buffer_id=ofproto.OFP_NO_BUFFER, actions=actions, data=data)
+  		      if out:
+  		        datapath.send_msg(out)
+                    
         return
         
         
@@ -199,3 +241,4 @@ class SimpleSwitch14(app_manager.RyuApp):
 			
 			
 			
+
